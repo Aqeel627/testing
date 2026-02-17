0c05027cdce73bb170cccfb3f4ec0ab1ecba4ac9
Muhammad Sami <samiqutham@gmail.com>
2026-02-17 13:54:14 +0500

    state-presists

--- src/app/(pages)/inplay/[sport]/index.tsx
+++ src/app/(pages)/inplay/[sport]/index.tsx
@@ -1,35 +1,11 @@
 "use client";
 
 import { useAppStore } from "@/lib/store/store";
-import React, { useEffect } from "react";
 
 const InplayPage = () => {
-  const { allEventsList } = useAppStore();
+  const { inplayEvents } = useAppStore();
 
-  const formattedEvents = React.useMemo(() => {
-    if (!allEventsList) return null;
-
-    const four = (allEventsList[4] || []).filter(
-      (item: any) => item?.inplay === true,
-    );
-
-    const two = (allEventsList[2] || []).filter(
-      (item: any) => item?.inplay === true,
-    );
-
-    const one = (allEventsList[1] || []).filter(
-      (item: any) => item?.inplay === true,
-    );
-
-    return {
-      all: [...four, ...two, ...one],
-      4: four,
-      2: two,
-      1: one,
-    };
-  }, [allEventsList]);
-
-  console.log(formattedEvents)
+  console.log(inplayEvents);
 
   return <div></div>;
 };
--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -12,17 +12,51 @@ export const useAppStore = create<any>()(
       menuList: null,
       exchangeNews: null,
       stakeValue: null,
+      selectedEventTypeId: null,
+      selectedBet: null,
+      inplayEvents:null,
 
       setCasinoEvents: (data: any) => set({ casinoEvents: data }),
       setExchangeTypeList: (data: any) => set({ exchangeTypeList: data }),
       setMenuList: (data: any) => set({ menuList: data }),
-      setAllEventsList: (data: any) => set({ allEventsList: data }),
       setExchangeNews: (data: any) => set({ exchangeNews: data }),
       setStakeValue: (data: any) => set({ stakeValue: data }),
       setUserBalance: (value: any) => set({ userBalance: value }),
+      setSelectedEventTypeId: (id: string) => set({ selectedEventTypeId: id }),
+      setSelectedBet: (data: any) => set({ selectedBet: data }),
+      clearSelectedBet: () => set({ selectedBet: null }),
+
+      setAllEventsList: (data: any) =>
+        set(() => {
+          if (!data) return { allEventsList: data, inplayEvents: null };
+
+          // descending keys order: 4 → 2 → 1
+          const keys = Object.keys(data)
+            .map(Number)
+            .sort((a, b) => b - a);
+
+          const result: any = {};
+          let all: any[] = [];
+
+          keys.forEach((key) => {
+            const filtered = (data[key] || []).filter(
+              (item: any) => item?.inplay,
+            );
+            result[key] = filtered;
+            all = [...all, ...filtered];
+          });
+
+          result.all = all;
+          result.totalLength = all.length;
+
+          return {
+            allEventsList: data,
+            inplayEvents: result,
+          };
+        }),
     }),
     {
       name: "app-storage", // localStorage key
-    }
-  )
+    },
+  ),
 );
