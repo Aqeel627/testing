a694a5598c59cd8cf2f0600ca1aafffffc6e7cf7
Muhammad Shahzeb <shahzebofficial27@gmail.com>
2026-02-20 19:27:01 +0500

    update bottom nav

--- src/components/common/bottom-nav/index.tsx
+++ src/components/common/bottom-nav/index.tsx
@@ -5,18 +5,22 @@ import Link from "next/link";
 import { useTheme } from "next-themes";
 import React, { Fragment, useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
+import CenterRadialButton from "@/components/common/bottom-nav/CenterRadialButton"; 
 
 const BottomNavbar = () => {
   const [isSafari, setIsSafari] = useState(false);
 
   const pathName = usePathname();
   const { theme, setTheme } = useTheme();
+
   const items = [
     { icon: "house", link: "/" },
     { icon: "inplay", link: "/inplay" },
-    { icon: "theme" },
-    { icon: "exchange", link: "#" },
-    { icon: "casino", link: "#" },
+    // { icon: "theme" },
+        { icon: "casinoic", link: "#" },
+    {icon:"bets", link: "#" },
+    // { icon: "exchange", link: "#" },
+
   ];
 
   useEffect(() => {
@@ -32,63 +36,33 @@ const BottomNavbar = () => {
   return (
     <div
       className={cn(
-        // gap-2 max-[330px]:gap-1  max-[354px]:gap-2 max-[380px]:gap-3 max-[440px]:gap-4
-        "md:hidden fixed border shadow-[0_8px_32px_rgba(0,0,0,0.2)]! bottom-5 left-1/2 -translate-x-1/2 px-2 py-2    w-[84%] flex justify-around items-center rounded-full glass h-15",
+        "md:hidden fixed border shadow-[0_8px_32px_rgba(0,0,0,0.2)]! bottom-5 left-1/2 -translate-x-1/2 px-2 py-2 w-[84%] flex justify-around items-center rounded-full glass h-15", 
         theme === "dark"
           ? "border-[rgba(255,255,255,0.3)]"
           : "border-[rgb(205,192,192,0.5)] bg-[linear-gradient(135deg,rgba(255,255,255,0.25),rgba(255,255,255,0.05))]! backdrop-blur-[20px]!",
         isSafari ? "bottom-2" : "bottom-5",
       )}
     >
-      {items?.map((item, idx) => (
-        <Fragment key={idx}>
-          {item?.link ? (
-            <Link
-              href={item.link}
-              className={cn(
-                "h-12 w-12 flex border border-[rgba(255,255,255,0.3)] justify-center items-center glass rounded-full",
-                pathName === item?.link
-                  ? "bg-(--nav-item-root-active-bg)! text-(--nav-item-root-active-color)"
-                  : "bg-(--IconButton-hoverBg)",
-                theme === "dark"
-                  ? "border-[rgba(255,255,255,0.3)]"
-                  : "border-[rgb(205,192,192,0.5)]",
-              )}
-            >
-              <Icon name={item.icon} width={25} height={25} />
-            </Link>
-          ) : (
-            <div
-              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-              className={cn(
-                "h-12 w-12 border border-[rgba(255,255,255,0.3)] flex justify-center items-center glass rounded-full bg-(--IconButton-hoverBg)",
-                theme === "dark"
-                  ? "border-[rgba(255,255,255,0.3)]"
-                  : "border-[rgb(205,192,192,0.5)]",
-              )}
-            >
-              {theme === "dark" ? (
-                <Icon
-                  name="moon"
-                  width={25}
-                  height={25}
-                  className="transition-transform"
-                />
-              ) : (
-                <img
-                  src="/sun.svg"
-                  alt="sun"
-                  className="w-6.25 h-6.25 transition-transform"
-                  style={{
-                    filter:
-                      "invert(67%) sepia(9%) saturate(354%) hue-rotate(174deg) brightness(70%) contrast(87%)",
-                  }}
-                />
-              )}
-            </div>
-          )}
-        </Fragment>
-      ))}
+{items.map((item, idx) => (
+  <Fragment key={idx}>
+    {idx === 2 && <CenterRadialButton />}
+
+    <Link
+      href={item.link}
+      className={cn(
+        "h-12 w-12 flex border justify-center items-center glass rounded-full",
+        pathName === item?.link
+          ? "bg-(--nav-item-root-active-bg)! text-(--nav-item-root-active-color)"
+          : "bg-(--IconButton-hoverBg)",
+        theme === "dark"
+          ? "border-[rgba(255,255,255,0.3)]"
+          : "border-[rgb(205,192,192,0.5)]"
+      )}
+    >
+      <Icon name={item.icon} width={25} height={25} />
+    </Link>
+  </Fragment>
+))}
     </div>
   );
 };
