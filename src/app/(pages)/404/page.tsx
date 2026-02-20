48ed2049a92b78048412c7e30c8a71743b2214a9
Muhammad Sami <samiqutham@gmail.com>
2026-02-20 19:23:36 +0500

    variables

--- src/app/(pages)/404/page.tsx
+++
@@ -1,8 +0,0 @@
-import NotFoundComponent from '@/components/common/not-found-component'
-import React from 'react'
-
-export default function PageNotFound() {
-  return (
-    <NotFoundComponent />
-  )
-}
