80a20d351e6b45aa525ba75bd4081dc77273c78d
Muhammad Sami <samiqutham@gmail.com>
2026-02-20 22:44:34 +0500

    final code

--- src/components/pages/live-casino/index.tsx
+++ src/components/pages/live-casino/index.tsx
@@ -30,9 +30,9 @@ export default function Casino({ hideHeading }: { hideHeading?: boolean }) {
         <div className="relative flex items-center justify-center w-full mb-6 px-2 min-[600px]:px-4">
           {/* Left Glowing Line & Dots */}
           {/* flex-1 lagaya hai taake line auto-stretch ho */}
-          <div className="flex relative items-center justify-end gap-1.5 min-[500px]:gap-2 w-full max-w-[80px] min-[400px]:max-w-[150px] min-[700px]:max-w-[250px]">
-            <div className="neon-underline -top-1">
-              <span className="neon-glow glow-main"></span>
+          <div className="flex relative items-center justify-end gap-2 w-full">
+            <div className="neon-underline -top-1 -right-1 max-sm:right-3 max-md:right-1">
+              {/* <span className="neon-glow glow-main"></span> */}
               <span className="neon-line line-main"></span>
 
               <span className="neon-glow glow-right"></span>
@@ -56,11 +56,11 @@ export default function Casino({ hideHeading }: { hideHeading?: boolean }) {
           </h2>
 
           {/* Right Glowing Line & Dots */}
-          <div className="relative flex items-center justify-start gap-1.5 min-[500px]:gap-2 w-full max-w-[80px] min-[400px]:max-w-[150px] min-[700px]:max-w-[250px]">
+          <div className="relative flex items-center justify-start gap-2 w-full">
             <div className="w-1 h-1 min-[500px]:w-[5px] min-[500px]:h-[5px] rounded-full bg-[#078dee] shadow-[0_0_8px_#078dee]"></div>
             <div className="w-1.25 h-1.25 bg-[#ffbbbb] dark:shadow-[0_0_6px_#ff3b3b,0_0_3px_#ff7f7f]"></div>
-            <div className="neon-underline -top-1">
-              <span className="neon-glow glow-main"></span>
+            <div className="neon-underline -top-1 -left-1 max-md:left-1 max-sm:left-3">
+              {/* <span className="neon-glow glow-main"></span> */}
               <span className="neon-line line-main"></span>
 
               <span className="neon-glow glow-left"></span>
