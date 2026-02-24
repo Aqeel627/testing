374f186d6f36fe040e02990d681ba8437692f8fd
Muhammad Sami <samiqutham@gmail.com>
2026-02-24 10:26:49 +0500

    add axios instance & stop gestures

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,7 +71,11 @@ export default function RootLayout({
         />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
         />
       </head>
       <body
--- src/lib/axios-instance.ts
+++ src/lib/axios-instance.ts
@@ -1,5 +1,7 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosHeaders, InternalAxiosRequestConfig } from "axios";
 import { BASE_URL_API } from "./config";
+import { useAuthStore } from "./useAuthStore";
+import Router from "next/router";
 
 export const http = axios.create({
   baseURL: BASE_URL_API ,
@@ -13,19 +15,63 @@ export const http = axios.create({
 // -----------------------
 // 🔥 Request Interceptor
 // -----------------------
-http.interceptors.request.use(
-  (config) => {
-    if (typeof window !== "undefined") {
-      const token = localStorage.getItem("token");
+// http.interceptors.request.use(
+//   (config) => {
+//     if (typeof window !== "undefined") {
+//       const token = localStorage.getItem("token");
 
-      if (token) {
-        config.headers["Authorization"] = `Bearer ${token}`;
-      }
-    }
+//       if (token) {
+//         config.headers["Authorization"] = `Bearer ${token}`;
+//       }
+//     }
 
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+if (typeof window !== "undefined") {
+    // Request Interceptor
+    http.interceptors.request.use(
+        (config: InternalAxiosRequestConfig) => {
+            const token = localStorage.getItem("token");
+            if (token) {
+                if (!config.headers) {
+                    config.headers = new AxiosHeaders();
+                }
+                (config.headers as AxiosHeaders).set(
+                    "Authorization",
+                    `Bearer ${token}`
+                );
+            }
+            return config;
+        },
+        (error: AxiosError) => Promise.reject(error)
+    );
+
+    // Response Interceptor
+    http.interceptors.response.use(
+        (response) => response,
+        (error: AxiosError) => {
+            if (error.response?.status === 401) {
+                // toast.error("Please log in again.", {
+                //     position: "top-right",
+                //     autoClose: 700,
+                // });
+
+                // Logout from Zustand store
+                const logout = useAuthStore.getState().logout;
+                logout();
+
+                const isDesktop = window.innerWidth > 1024;
+                Router.push(isDesktop ? "/" : "/authentication/login");
+            }
+
+            return Promise.reject(error);
+        }
+    );
+}
+
+export default http;
