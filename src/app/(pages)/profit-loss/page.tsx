4f4ca5ed299bc5a82cdde73c048d3c56dc87b4ff
Aqeel Anwar <aqeelanwar677@gmail.com>
2026-02-26 16:08:09 +0500

    Add Profit & Loss pages and implement data fetching logic

---
+++ src/app/(pages)/profit-loss/page.tsx
@@ -0,0 +1,12 @@
+import ProfitLossPage from '@/components/pages/profit-loss-page'
+import ProfitLossEventPage from '@/components/pages/profitloss-event-page'
+import ProfitLossMarketPage from '@/components/pages/profitloss-market-page'
+import React from 'react'
+
+export default function ProfitLoss() {
+  return (
+    <ProfitLossPage></ProfitLossPage>
+    // <ProfitLossEventPage></ProfitLossEventPage>
+    // <ProfitLossMarketPage></ProfitLossMarketPage>
+  )
+}
