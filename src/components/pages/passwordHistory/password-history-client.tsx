bf2ef72f1a375e1fe29c80e89a4311f48515b985
Muhammad Shahzeb <shahzebofficial27@gmail.com>
2026-02-26 15:01:24 +0500

    update

--- src/components/pages/passwordHistory/password-history-client.tsx
+++ src/components/pages/passwordHistory/password-history-client.tsx
@@ -42,7 +42,7 @@ export default function PasswordHistoryClient() {
   return (
     <div className="">
       <div className="">
-        <ul className="flex justify-start  gap-3 w-full overflow-x-auto scrollbar-hide py-2">
+        <ul className="flex justify-start  gap-3 w-full overflow-x-auto scrollbar-hide">
           <li>
             <Link
               href="/activity"
