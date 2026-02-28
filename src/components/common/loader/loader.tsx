b3f3498a5bd83e55c4f8ff514ad6d18f9c88a2f3
Muhammad Sami <samiqutham@gmail.com>
2026-02-28 15:11:05 +0500

    add id on common components

--- src/components/common/loader/loader.tsx
+++ src/components/common/loader/loader.tsx
@@ -10,32 +10,32 @@ const Loader = () => {
   const { theme } = useTheme();
 
   return (
-    <div className={styles.loaderWrapper}>
-      <div
-        className={cn(
-          theme === "dark"
-            ? "apple-glass apple-glass-dark"
-            : "apple-glass-light",
-          styles.cssContainer
-        )}
-      >
-<div className={styles.ringContainer}>
+    <div id="loader.tsx">
+      <div className={styles.loaderWrapper}>
+        <div
+          className={cn(
+            theme === "dark"
+              ? "apple-glass apple-glass-dark"
+              : "apple-glass-light",
+            styles.cssContainer,
+          )}
+        >
+          <div className={styles.ringContainer}>
+            <span className={styles.ringTrack}></span>
 
-  <span className={styles.ringTrack}></span>
+            <span className={`${styles.ringArc} ${styles.arc1}`}></span>
+            <span className={`${styles.ringArc} ${styles.arc2}`}></span>
+            <span className={`${styles.ringArc} ${styles.arc3}`}></span>
+            <span className={`${styles.ringArc} ${styles.arc4}`}></span>
+            <span className={`${styles.ringArc} ${styles.arc5}`}></span>
+            <span className={`${styles.ringArc} ${styles.arc6}`}></span>
 
-  <span className={`${styles.ringArc} ${styles.arc1}`}></span>
-  <span className={`${styles.ringArc} ${styles.arc2}`}></span>
-  <span className={`${styles.ringArc} ${styles.arc3}`}></span>
-  <span className={`${styles.ringArc} ${styles.arc4}`}></span>
-  <span className={`${styles.ringArc} ${styles.arc5}`}></span>
-  <span className={`${styles.ringArc} ${styles.arc6}`}></span>
+            <span className={styles.ringGlow}></span>
+            <span className={styles.innerThinRing}></span>
+            {/* <span className={styles.innerThin}></span> */}
+            <div className={styles.ringInner}></div>
 
-  <span className={styles.ringGlow}></span>
-<span className={styles.innerThinRing}></span>
-  {/* <span className={styles.innerThin}></span> */}
-  <div className={styles.ringInner}></div>
-
-  {/* <div className={styles.logoSlot}>
+            {/* <div className={styles.logoSlot}>
     <Link href="/" aria-label="Logo">
       <Image
         src={theme === "dark" ? "/logo-black.svg" : "/logo-white.svg"}
@@ -46,25 +46,25 @@ const Loader = () => {
       />
     </Link>
   </div> */}
-  <span className={styles.logoAnim}>
-  <Link
-    href="/"
-    aria-label="Logo"
-    className={styles.loaderImageContainer}
-  >
-    <Image
-      src={theme === "dark" ? "/logo-black.svg" : "/logo-white.svg"}
-      alt="brand logo"
-      width={152}
-      height={44}
-      priority
-      sizes="152px"
-      className={styles.LoaderImg}
-    />
-  </Link>
-</span>
-
-</div>
+            <span className={styles.logoAnim}>
+              <Link
+                href="/"
+                aria-label="Logo"
+                className={styles.loaderImageContainer}
+              >
+                <Image
+                  src={theme === "dark" ? "/logo-black.svg" : "/logo-white.svg"}
+                  alt="brand logo"
+                  width={152}
+                  height={44}
+                  priority
+                  sizes="152px"
+                  className={styles.LoaderImg}
+                />
+              </Link>
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
