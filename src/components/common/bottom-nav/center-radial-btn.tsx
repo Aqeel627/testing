be56579f28b602110cc1c01faebd9a513ceb2a9a
Aqeel Anwar <aqeelanwar677@gmail.com>
2026-03-07 14:42:22 +0500

    changes

--- src/components/common/bottom-nav/center-radial-btn.tsx
+++ src/components/common/bottom-nav/center-radial-btn.tsx
@@ -181,8 +181,8 @@ const CenterRadialButton = () => {
     } else if (iconName === "stake") {
       router.push("/settings");
     } else if (iconName === "powerOff") {
-    logout();
-    router.push("/");
+      logout();
+      router.push("/");
     }
   };
 
@@ -190,120 +190,121 @@ const CenterRadialButton = () => {
 
   return (
     <div id="centerRadialButton.tsx">
-    <div
-      ref={containerRef}
-      className="relative flex items-center justify-center pointer-events-none bottom-[-11px]"
-    >
-      <div ref={rootRef} className="relative w-12 h-12 mx-auto">
-        {/* GLASS SEMI CIRCLE BACKGROUND */}
-        <div
-          className={cn(
-            "absolute left-1/2 -translate-x-1/2 bottom-[64px] h-[125px] max-[360px]:h-[118px] overflow-hidden transition-all duration-300",
-            "w-[74vw] max-[405px]:w-[73vw] max-[390px]:w-[72vw] max-[375px]:w-[71vw] min-[426px]:w-[325px]",
-            open
-              ? "opacity-100 scale-100 visible"
-              : "opacity-0 scale-95 invisible",
-          )}
-        >
+      <div
+        ref={containerRef}
+        className="relative flex items-center justify-center pointer-events-none bottom-[-11px]"
+      >
+        <div ref={rootRef} className="relative w-12 h-12 mx-auto">
+          {/* GLASS SEMI CIRCLE BACKGROUND */}
           <div
-            ref={arcCircleRef}
             className={cn(
-              "absolute left-1/2 top-0 w-full aspect-square -translate-x-1/2 rounded-full glass-radial border",
-              theme !== "dark" && "light-radial",
+              "absolute left-1/2 -translate-x-1/2 bottom-[64px] h-[125px] max-[360px]:h-[118px] overflow-hidden transition-all duration-300",
+              "w-[74vw] max-[405px]:w-[73vw] max-[390px]:w-[72vw] max-[375px]:w-[71vw] min-[426px]:w-[325px]",
+              open
+                ? "opacity-100 scale-100 visible"
+                : "opacity-0 scale-95 invisible",
+            )}
+          >
+            <div
+              ref={arcCircleRef}
+              className={cn(
+                "absolute left-1/2 top-0 w-full aspect-square -translate-x-1/2 rounded-full glass-radial border",
+                theme !== "dark" && "light-radial",
+                theme === "dark"
+                  ? "border-[rgba(255,255,255,0.25)]"
+                  : "border-[rgba(255,255,255,0.4)]",
+              )}
+              style={{ transformOrigin: "center top" }}
+            />
+          </div>
+
+          {/* RADIAL ICONS */}
+          {rotations.map((deg, i) => {
+            const transformOpen = useFixed425To767
+              ? // EXACT 425px values (no change from 425 to 767)
+              `translateX(calc(${xPerc425[i]}% + 0px)) translateY(${y425[i]}px) rotate(${deg}deg) translateY(-${r425}px) rotate(${-deg}deg)`
+              : // <425px: border gap == icon gap == (10..20px)
+              `translateX(calc(-50% + ${small.dx}px)) translateY(${small.baseY}px) rotate(${small.angles[i]}deg) translateY(-${small.rCenter}px) rotate(${-small.angles[i]}deg)`;
+            const iconName =
+              radialIcons[i] === "stake"
+                ? theme === "light"
+                  ? "stakeLight"
+                  : "stakeDark"
+                : radialIcons[i] === "themeSetting"
+                  ? theme === "light"
+                    ? "themeSettingLight"
+                    : "themeSettingDark"
+                  : radialIcons[i];
+            const iconClass = `icon-${iconName}`;
+            return (
+              <span
+                key={i}
+                // className="absolute left-1/2 bottom-0 pointer-events-auto transition-transform duration-500"
+                className={`absolute left-1/2 bottom-0 pointer-events-auto transition-transform duration-500 radial-icon ${iconClass}`}
+                style={{
+                  transform: open
+                    ? transformOpen
+                    : `translateX(-50%) translateY(-22px)`,
+                  transitionDelay: open ? `${i * 0.08}s` : "0s",
+                }}
+              >
+                {radialIcons[i] === "theme" ? (
+                  <button
+                    ref={sampleIconRef}
+                    type="button"
+                    aria-label="Open menu"
+                    onClick={() => {
+                      setOpen(false);
+                      setTheme(theme === "dark" ? "light" : "dark");
+                    }}
+                    className={cn(
+                      "c333 w-[50px] h-[50px] max-[355px]:w-[45px] max-[355px]:h-[45px] max-[330px]:h-[42px] rounded-full flex items-center justify-center  btn-glass backdrop-blur-[20px] border shadow-lg transition-opacity duration-300",
+                      open ? "opacity-100" : "opacity-0",
+                      theme === "dark"
+                        ? "border-[rgba(255,255,255,0.25)]"
+                        : "border-[rgba(255,255,255,0.4)]",
+                    )}
+                  >
+                    {theme === "dark" ? (
+                      <Icon name="moon" width={22} height={22} />
+                    ) : (
+                      <Icon name="moonOutline" width={22} height={22} />
+                    )}
+                  </button>
+                ) : (
+                  <div
+                    onClick={() => handleIconAction(radialIcons[i])}
+                    className={cn(
+                      "w-[50px] h-[50px] max-[355px]:w-[45px] max-[355px]:h-[45px] rounded-full flex items-center justify-center btn-glass backdrop-blur-[20px] border shadow-lg transition-opacity duration-300",
+                      open ? "opacity-100" : "opacity-0",
+                      theme === "dark"
+                        ? "border-[rgba(255,255,255,0.25)]"
+                        : "border-[rgba(255,255,255,0.4)]",
+                    )}
+                  >
+                    <Icon name={iconName} width={22} height={22} />
+                  </div>
+                )}
+              </span>
+            );
+          })}
+          {/* CENTER BUTTON */}
+          <button
+            onClick={() => setOpen(!open)}
+            className={cn(
+              "absolute left-1/2 bottom-0 w-12 h-12 rounded-full pointer-events-auto z-10000 flex items-center justify-center transition-all duration-300 btn-glass backdrop-blur-[25px] border shadow-[0_8px_25px_rgba(0,0,0,0.25)]",
               theme === "dark"
-                ? "border-[rgba(255,255,255,0.25)]"
+                ? "border-[rgba(255,255,255,0.3)]"
                 : "border-[rgba(255,255,255,0.4)]",
             )}
-            style={{ transformOrigin: "center top" }}
-          />
-        </div>
-
-        {/* RADIAL ICONS */}
-        {rotations.map((deg, i) => {
-          const transformOpen = useFixed425To767
-            ? // EXACT 425px values (no change from 425 to 767)
-            `translateX(calc(${xPerc425[i]}% + 0px)) translateY(${y425[i]}px) rotate(${deg}deg) translateY(-${r425}px) rotate(${-deg}deg)`
-            : // <425px: border gap == icon gap == (10..20px)
-            `translateX(calc(-50% + ${small.dx}px)) translateY(${small.baseY}px) rotate(${small.angles[i]}deg) translateY(-${small.rCenter}px) rotate(${-small.angles[i]}deg)`;
-          const iconName =
-            radialIcons[i] === "stake"
-              ? theme === "light"
-                ? "stakeLight"
-                : "stakeDark"
-              : radialIcons[i] === "themeSetting"
-                ? theme === "light"
-                  ? "themeSettingLight"
-                  : "themeSettingDark"
-                : radialIcons[i];
-          const iconClass = `icon-${iconName}`;
-          return (
-            <span
-              key={i}
-              // className="absolute left-1/2 bottom-0 pointer-events-auto transition-transform duration-500"
-              className={`absolute left-1/2 bottom-0 pointer-events-auto transition-transform duration-500 radial-icon ${iconClass}`}
-              style={{
-                transform: open
-                  ? transformOpen
-                  : `translateX(-50%) translateY(-22px)`,
-                transitionDelay: open ? `${i * 0.08}s` : "0s",
-              }}
-            >
-              {radialIcons[i] === "theme" ? (
-                <button
-                  ref={sampleIconRef}
-                  type="button"
-                  onClick={() => {
-                    setOpen(false);
-                    setTheme(theme === "dark" ? "light" : "dark");
-                  }}
-                  className={cn(
-                    "c333 w-[50px] h-[50px] max-[355px]:w-[45px] max-[355px]:h-[45px] max-[330px]:h-[42px] rounded-full flex items-center justify-center  btn-glass backdrop-blur-[20px] border shadow-lg transition-opacity duration-300",
-                    open ? "opacity-100" : "opacity-0",
-                    theme === "dark"
-                      ? "border-[rgba(255,255,255,0.25)]"
-                      : "border-[rgba(255,255,255,0.4)]",
-                  )}
-                >
-                  {theme === "dark" ? (
-                    <Icon name="moon" width={22} height={22} />
-                  ) : (
-                    <Icon name="moonOutline" width={22} height={22} />
-                  )}
-                </button>
-              ) : (
-                <div
-                  onClick={() => handleIconAction(radialIcons[i])}
-                  className={cn(
-                    "w-[50px] h-[50px] max-[355px]:w-[45px] max-[355px]:h-[45px] rounded-full flex items-center justify-center btn-glass backdrop-blur-[20px] border shadow-lg transition-opacity duration-300",
-                    open ? "opacity-100" : "opacity-0",
-                    theme === "dark"
-                      ? "border-[rgba(255,255,255,0.25)]"
-                      : "border-[rgba(255,255,255,0.4)]",
-                  )}
-                >
-                  <Icon name={iconName} width={22} height={22} />
-                </div>
-              )}
+            style={{ transform: "translateX(-50%) translateY(-11px)" }}
+          >
+            <span className="text-[30px] font-medium transition-transform duration-300 relative bottom-[2px]">
+              {open ? "×" : "+"}
             </span>
-          );
-        })}
-        {/* CENTER BUTTON */}
-        <button
-          onClick={() => setOpen(!open)}
-          className={cn(
-            "absolute left-1/2 bottom-0 w-12 h-12 rounded-full pointer-events-auto z-10000 flex items-center justify-center transition-all duration-300 btn-glass backdrop-blur-[25px] border shadow-[0_8px_25px_rgba(0,0,0,0.25)]",
-            theme === "dark"
-              ? "border-[rgba(255,255,255,0.3)]"
-              : "border-[rgba(255,255,255,0.4)]",
-          )}
-          style={{ transform: "translateX(-50%) translateY(-11px)" }}
-        >
-          <span className="text-[30px] font-medium transition-transform duration-300 relative bottom-[2px]">
-            {open ? "×" : "+"}
-          </span>
-        </button>
+          </button>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
